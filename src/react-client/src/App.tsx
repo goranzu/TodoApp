8338406ca3f647e5e10c0f10b12cbdb48376752b
Goran Tenic <tenic.g@gmail.com>
2023-11-11 19:25:44 +0100

    server test

--- src/react-client/src/App.tsx
+++ src/react-client/src/App.tsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("/api");
+      if (response.ok) {
+        const data = await response.text();
+        setMessage(data);
+      }
+    }
+
+    getData();
+  }, []);
 
   return (
     <>
       <div>
+        <h1>From Server: {message.length > 0 ? message : "No message yet!"}</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
